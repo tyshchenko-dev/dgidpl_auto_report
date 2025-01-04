dd34b956fdf55982801d109d7b0704b6de9517a8
Vladyslav Tyshchenko <vlad.tiw27@gmail.com>
2025-01-04 19:34:57 +0100

    update: add a simple starter menu for the script

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import pandas as pd
-import os
-from datetime import datetime
+import questionary
+import emoji
 
-folder_path = os.path.dirname(os.path.realpath(__file__))
 
-reports_folder = os.path.join(folder_path, "reports")
+def mode_one():
+    print(f"{emoji.emojize(':hammer_and_wrench:')} Обрано режим 1: Створення звіту на основі всіх файлів.")
 
-files = os.listdir(reports_folder)
 
-dataframes = [pd.read_excel(os.path.join(reports_folder, file)) for file in files if file != ".DS_Store"]
-combined_df = pd.concat(dataframes, ignore_index=True)
+def mode_two():
+    print(f"{emoji.emojize(':bar_chart:')} Обрано режим 2: Створення звіту по даті і області.")
+    setting1 = input("Введіть області які вас цікавлять через кому (приклад: Київська, Херсонська, Вінницька): ")
+    setting2 = input("Введіть місяць та рік на основі яких формувати звіт в форматі 01_2025-05_2025: ")
+    print(f"Режим 2: налаштування1={setting1}, налаштування2={setting2}")
 
-result = combined_df.groupby("Критерії", as_index=False)[
-    ["К-сть де є CR", "К-сть де немає CR", "Загальна к-сть"]
-].sum()
 
-current_date = datetime.now().strftime("%m_%d_%Y")
-result_file_name = f"dgidpl_report_summary_{current_date}.xlsx"
 
-result.to_excel(result_file_name, index=False)
+def main():
+    greeting = [
+        f"{emoji.emojize(':police_officer:')} Привіт, це скрипт який допомагає зформувати статистику затримань по всій Україні.",
+        f"{emoji.emojize(':classical_building:')} Розроблений для Департаменту головної інспекції та дотримання прав людини."
+    ]
+    print(greeting[0])
+    print(greeting[1])
 
-print(f"Файл з результатом збережений: {result_file_name}")
+    choice = questionary.select(
+        "Оберіть режим роботи скрипта:",
+        choices=[
+            "Створення звіту на основі всіх файлів",
+            "Створення звіту по даті і області",
+        ]).ask()
+
+    if choice == "Звіт на основі всіх файлів":
+        mode_one()
+    elif choice == "Звіт по даті та області":
+        mode_two()
+    else:
+        print("Помилка.")
+
+
+if __name__ == "__main__":
+    main()
