322876fd31ecc10311cb7288554df0e6e95b838d
Vladyslav Tyshchenko <vlad.tiw27@gmail.com>
2025-01-05 10:30:55 +0100

    refactor(parse_date): add docstring, implement exception handling, create custom error class, and add validations

--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import logging
 from datetime import datetime
 
 
@@ -20,27 +21,58 @@ def search_files(files, selected_regions, start_month, end_month, start_year, en
     return filtered_files
 
 def summ_files(files, reports_folder):
-    dataframes = [pd.read_excel(os.path.join(reports_folder, file)) for file in files if file != ".DS_Store"]
+    dataframes = [pd.read_excel(os.path.join(reports_folder, file)) for file in files]
     combined_df = pd.concat(dataframes, ignore_index=True)
     result = combined_df.groupby("Критерії", as_index=False)[
         ["К-сть де є CR", "К-сть де немає CR", "Загальна к-сть"]
     ].sum()
     return result
 
+class DateFormatError(Exception):
+    """Exception raised when a date format is incorrect."""
+    pass
+
+
 def parse_date(date):
-    chunks = date.split("-")
-    start_date = chunks[0]
-    end_date = chunks[1]
-    start_month = int(start_date.split("_")[0])
-    start_year = int(start_date.split("_")[1])
-    end_month = int(end_date.split("_")[0])
-    end_year = int(end_date.split("_")[1])
-    return {
-        "start_month": start_month,
-        "start_year": start_year,
-        "end_month": end_month,
-        "end_year": end_year,
-    }
+    """
+    Split date string "MM_YYYY-MM_YYYY" and return dict with date components.
+
+    :param date: Date string in format "MM_YYYY-MM_YYYY", example, "01_2020-12_2020".
+    :return: Dict with keys 'start_month', 'start_year', 'end_month', 'end_year'.
+    :raises DateFormatError: If date format is invalid.
+    """
+    try:
+        chunks = date.split("-")
+        if len(chunks) != 2:
+            raise DateFormatError
+
+        start_date, end_date = chunks
+
+        start_parts = start_date.split("_")
+        end_parts = end_date.split("_")
+
+        if len(start_parts) != 2 or len(end_parts) != 2:
+            raise DateFormatError("Each date part must contains month and year, splited by dash")
+
+        start_month, start_year = map(int, start_parts)
+        end_month, end_year = map(int, end_parts)
+
+        if not(start_month >= 1 or start_month >= 12):
+            raise DateFormatError("Start month must be in range 1-12")
+        if not(end_month >= 1 or end_month <= 12):
+            raise DateFormatError("End month must be in range 1-12")
+        if start_year > end_year or (start_year == end_year and start_month > end_month):
+            raise DateFormatError("Start date must be before end date")
+
+        return {
+            "start_month": start_month,
+            "start_year": start_year,
+            "end_month": end_month,
+            "end_year": end_year,
+        }
+    except (ValueError, IndexError) as e:
+        raise DateFormatError("Cannot parse date, invalid format {date}") from e
+
 
 def generate_report(settings = None):
     print("Generating report...")
@@ -50,21 +82,22 @@ def generate_report(settings = None):
     reports_folder = os.path.join(folder_path, "reports")
 
     files = os.listdir(reports_folder)
+    files = [file for file in files if file.endswith(".xlsx")]
 
-    if settings:
-        date = settings["date"]
-        selected_regions = settings["selected_regions"]
-        print(selected_regions)
-        parsed_date = parse_date(date)
-        print(parsed_date)
-        files = search_files(files, selected_regions, parsed_date["start_month"], parsed_date["end_month"], parsed_date["start_year"], parsed_date["end_year"])
+    print("files:", files)
 
-    if files:
-        result = summ_files(files, reports_folder)
-        current_date = datetime.now().strftime("%m_%d_%Y")
-        result_file_name = f"dgidpl_report_summary_{current_date}.xlsx"
-        result.to_excel(result_file_name, index=False)
-        print(f"Файл з результатом збережений: {result_file_name}")
+    # if settings:
+    #     date = settings["date"]
+    #     selected_regions = settings["selected_regions"]
+    #     parsed_date = parse_date(date)
+    #     files = search_files(files, selected_regions, parsed_date["start_month"], parsed_date["end_month"], parsed_date["start_year"], parsed_date["end_year"])
+    #
+    # if files:
+    #     result = summ_files(files, reports_folder)
+    #     current_date = datetime.now().strftime("%m_%d_%Y")
+    #     result_file_name = f"dgidpl_report_summary_{current_date}.xlsx"
+    #     result.to_excel(result_file_name, index=False)
+    #     print(f"Файл з результатом збережений: {result_file_name}")
 
 
 generate_report()
